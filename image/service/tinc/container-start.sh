6eb9c22554e1ca23a58aa3b4cd97821590657900
Bertrand Gouny <bgouny@hipay.com>
2015-12-16 16:53:36 +0100

    init

---
+++ image/service/tinc/container-start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+FIRST_START_DONE="/etc/docker-tinc-first-start-done"
+
+# container first start
+if [ ! -e "$FIRST_START_DONE" ]; then
+
+  /usr/sbin/tinc init $TINC_HOSTNAME
+
+  # add root user on specified networks
+  TINC_RUN_BEFORE_START_COMMANDS=($TINC_RUN_BEFORE_START_COMMANDS)
+  for command in "${TINC_RUN_BEFORE_START_COMMANDS[@]}"
+  do
+    echo "Run tinc command: ${!command}"
+    /usr/sbin/tinc ${!command}
+  done
+
+fi
+
+exit 0
